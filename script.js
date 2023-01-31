fd49fb1485191f5983dd904f234666be8002948f
TheFred94 <frederikrlarsen@gmail.com>
2023-01-31 22:39:41 +0100

    filter not working yet. No errors

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 "use strict";
-
+let movies;
+let filter = "all";
 const file = "actors.json";
 
-async function getdata(file) {
-  const result = await fetch(file);
-  const json = await result.json();
-  show(json);
-}
+const filterBtn = document.querySelectorAll("nav button");
+filterBtn.forEach((button) => button.addEventListener("click", filterMovies));
 
 document.querySelector(".luk").addEventListener("click", () => (popup.style.display = "none"));
 
+async function getdata(file) {
+  const result = await fetch(file);
+  movies = await result.json();
+  showMovies();
+}
+
+function filterMovies() {
+  filter = this.dataset.category;
+  document.querySelector(".clicked").classList.remove("clicked");
+  this.classList.add("clicked");
+  showMovies();
+}
+
 // Loads the json file and displays the data according to the parameters in the file
-function show(json) {
-  console.log(json);
+function showMovies() {
   const container = document.querySelector("main");
-  const template = document.querySelector("template");
-  json.forEach((actor) => {
-    const clone = template.cloneNode(true).content;
-    clone.querySelector(".actor").textContent = actor.fullname;
-    clone.querySelector(".movie").textContent = actor.movie;
-    clone.querySelector("article").addEventListener("click", () => showDetails(actor));
-    container.appendChild(clone);
+  const template = document.querySelector("template").content;
+  movies.forEach((movie) => {
+    console.log(movie);
+    if (filter == movie.category || filter == "all") {
+      const clone = template.cloneNode(true);
+      clone.querySelector(".actor").textContent = movie.fullname;
+      clone.querySelector(".movie").textContent = movie.movie;
+      clone.querySelector("article").addEventListener("click", () => showDetails(movie));
+      container.appendChild(clone);
+    }
   });
 }
 
