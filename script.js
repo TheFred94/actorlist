f65600998c80a5ba0235a32cf84447013eb94c2f
TheFred94 <frederikrlarsen@gmail.com>
2023-02-01 18:41:38 +0100

    filtrering virker

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let actors;
 let filter = "all";
-const = 
+const actorData = "actors.json";
 const header = document.querySelector("h1");
 
 // Click event on nav which runs filterActors function when clicked
@@ -12,17 +12,47 @@ document.querySelector(".close").addEventListener("click", () => (popup.style.di
 
 // Filters the actors and removes the class from the element and adds it to the new clicked element
 function filterActors() {
-  filter = this.dataset.category;
+  filter = this.dataset.movie;
   document.querySelector(".clicked").classList.remove("clicked");
   this.classList.add("clicked");
-  showActors()
+  header.textContent = this.textContent;
+  showActors(actors);
+  console.log("Actors filtered");
 }
-
-// Targets the "main" tag in the DOM
-const main = document.querySelector("main");
 
 // This retrieves the data from the json file
 async function getActorData() {
-const response = await fetch
-
+  console.log(actorData);
+  const response = await fetch(actorData);
+  actors = await response.json();
+  console.log("Actors shown");
+  showActors(actors);
 }
+
+// runs the showActors function from the getActorData function
+function showActors(actors) {
+  const container = document.querySelector("main");
+  const template = document.querySelector("template").content;
+  container.textContent = "";
+  console.log("content removed");
+  actors.forEach((actor) => {
+    if (filter == actor.movie || filter == "all") {
+      console.log("Actor data");
+      const clone = template.cloneNode(true);
+      clone.querySelector(".actor_name").textContent = actor.fullname;
+      clone.querySelector(".movie_name").textContent = actor.movie;
+      clone.querySelector("article").addEventListener("click", () => showActorDetails(actor));
+      container.appendChild(clone);
+      console.log("New actors");
+    }
+  });
+}
+
+function showActorDetails(actor) {
+  console.log(actor);
+  popup.style.display = "block";
+  document.querySelector(".actor_name").textContent = actor.fullname;
+  document.querySelector(".movie_name").textContent = actor.movie;
+}
+
+getActorData(actorData);
